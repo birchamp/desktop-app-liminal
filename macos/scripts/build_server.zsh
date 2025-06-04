979ee838f1f8e56a0621bc8c6f56abcda0c91c2c
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-06-04 14:00:57 -0500

    zsh scripts converted from bsh

--- macos/scripts/build_server.zsh
+++ macos/scripts/build_server.zsh
@@ -1,19 +1,15 @@
 #!/usr/bin/env zsh
 
-set -e
-set -u
+./clean.zsh
 
 if [ ! -f ../../local_server/target/release/local_server ]; then
     echo "Building local server"
     cd ../../local_server
     OPENSSL_STATIC=yes cargo build --release
-    cd ../macos/scripts
+    cd ../linux/scripts
 fi
 
-echo "Assembling build environment"
-node ./build.js
-
-echo "Running..."
-cd ../build
-chmod 755 liminal.zsh
-./liminal.zsh
+if [ ! -d ../build ]; then
+  echo "Assembling build environment"
+  node ./build.js
+fi
---
+++ macos/scripts/open.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+source ../../app_config.env
+
+set -e
+set -u
+
+# set port environment variable
+export ROCKET_PORT=19119
+
+if [ -d ../build ]; then
+  echo "Removing last build environment"
+  rm -rf ../build
+fi
+
+if [ ! -d ../build ]; then
+  echo "Assembling build environment"
+  node ./build.js
+fi
+
+echo "Running and Opening Browser..."
+cd ../build
+chmod +x $APP_NAME
+./$APP_NAME
